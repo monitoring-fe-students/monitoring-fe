4f4e4550da1ebca9b954fe25dd3dbb09d387b130
Laco <ladislav.dosa@gmail.com>
2023-05-22 13:14:46 +0200

    mock values added to azure service

---
+++ src/app/models/azure-rest-api.model.ts
@@ -0,0 +1,13 @@
+export enum Instance {
+  VMSProduction= "VMS Production",
+  CSMSProduction= "CSMS Production",
+  VMSQuality= "VMS Quality",
+  CSMSQuality= "CSMS Quality",
+}
+
+export enum Metric {
+  CPULoad= "CPU Load",
+  RAMRemainingMB= "RAM Remaining MB",
+  HDD1RemainingMB= "HDD1 Remaining MB",
+  HDD2RemainingMB= "HDD2 Remaining MB",
+}
