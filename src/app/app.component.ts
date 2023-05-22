dc6f46569c0c23d13f101e8fe36c24a76fcb6c65
Laco <ladislav.dosa@gmail.com>
2023-05-22 10:11:35 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AzureRestApiService } from 'src/services/azure-rest-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'monitoring-fe';
+export class AppComponent implements OnInit {
+  public loadAverage: number = 0;
+
+  constructor(private azureRestApiService: AzureRestApiService) { }
+  
+  ngOnInit(): void {
+  }  
 }
--- src/services/azure-rest-api.service.ts
+++ src/services/azure-rest-api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class AzureRestApiService {
 
   constructor() { }
+
+  getCurrentLoadAverage(vm: string): Observable<number> {
+    return of(0.5);    
+  }
 }
