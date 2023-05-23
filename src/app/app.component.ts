006eca420028dd87fb46cef4e20bb21870cd2697
David <svancardavid@gmail.com>
2023-05-23 11:05:44 +0200

    Premenné

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,58 @@ import { Instance, Metric } from './models/azure-rest-api.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public loadAverage: number = 0;
-  public HDD1: number = 0;
-  public HDD2: number = 0;
-  public RAM: number = 0;
+  // CSMS Production
+  public CPULoadCP: number = 0;
+  public HDD1CP: number = 0;
+  public HDD2CP: number = 0;
+  public RAMCP: number = 0;
+
+  // VMS Production
+  public CPULoadVP: number = 0;
+  public HDD1VP: number = 0;
+  public HDD2VP: number = 0;
+  public RAMVP: number = 0;
+
+  // CSMS Quality
+  public CPULoadCQ: number = 0;
+  public HDD1CQ: number = 0;
+  public HDD2CQ: number = 0;
+  public RAMCQ: number = 0;
+
+  // VMS Quality
+  public CPULoadVQ: number = 0;
+  public HDD1VQ: number = 0;
+  public HDD2VQ: number = 0;
+  public RAMVQ: number = 0;
 
 
   
   constructor(private azureRestApiService: AzureRestApiService) { }
   
   ngOnInit(): void {
-    this.azureRestApiService.getInstanceMetric(Instance.CSMSProduction, Metric.CPULoad).subscribe((data) => this.loadAverage = data)
-    this.azureRestApiService.getInstanceMetric(Instance.CSMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.HDD1 = data)
-    this.azureRestApiService.getInstanceMetric(Instance.CSMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.HDD2 = data)
-    this.azureRestApiService.getInstanceMetric(Instance.CSMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.RAM = data)
+      // CSMS Production
+    this.azureRestApiService.getInstanceMetric(Instance.CSMSProduction, Metric.CPULoad).subscribe((data) => this.CPULoadCP = data)
+    this.azureRestApiService.getInstanceMetric(Instance.CSMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.HDD1CP = data)
+    this.azureRestApiService.getInstanceMetric(Instance.CSMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.HDD2CP = data)
+    this.azureRestApiService.getInstanceMetric(Instance.CSMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.RAMCP = data)
+
+      // VMS Production
+    this.azureRestApiService.getInstanceMetric(Instance.VMSProduction, Metric.CPULoad).subscribe((data) => this.CPULoadVP = data)
+    this.azureRestApiService.getInstanceMetric(Instance.VMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.HDD1VP = data)
+    this.azureRestApiService.getInstanceMetric(Instance.VMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.HDD2VP = data)
+    this.azureRestApiService.getInstanceMetric(Instance.VMSProduction, Metric.HDD1RemainingMB).subscribe((data) => this.RAMVP = data)
+
+    // CSMS Quality
+    this.azureRestApiService.getInstanceMetric(Instance.CSMSQuality, Metric.CPULoad).subscribe((data) => this.CPULoadCQ = data)
+    this.azureRestApiService.getInstanceMetric(Instance.CSMSQuality, Metric.HDD1RemainingMB).subscribe((data) => this.HDD1CQ = data)
+    this.azureRestApiService.getInstanceMetric(Instance.CSMSQuality, Metric.HDD1RemainingMB).subscribe((data) => this.HDD2CQ = data)
+    this.azureRestApiService.getInstanceMetric(Instance.CSMSQuality, Metric.HDD1RemainingMB).subscribe((data) => this.RAMCQ = data)
+
+    // VMS Quality
+    this.azureRestApiService.getInstanceMetric(Instance.VMSQuality, Metric.CPULoad).subscribe((data) => this.CPULoadVQ = data)
+    this.azureRestApiService.getInstanceMetric(Instance.VMSQuality, Metric.HDD1RemainingMB).subscribe((data) => this.HDD1VQ = data)
+    this.azureRestApiService.getInstanceMetric(Instance.VMSQuality, Metric.HDD1RemainingMB).subscribe((data) => this.HDD2VQ = data)
+    this.azureRestApiService.getInstanceMetric(Instance.VMSQuality, Metric.HDD1RemainingMB).subscribe((data) => this.RAMVQ = data)
   }  
 }
 
