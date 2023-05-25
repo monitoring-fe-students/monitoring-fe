e0f86cc9faeea4009f1d91bd1613fbdb100f74a8
Laco <ladislav.dosa@gmail.com>
2023-05-25 09:33:34 +0200

    subscribe/unsubscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AzureRestApiService } from 'src/services/azure-rest-api.service';
 import { Instance, Metric } from './models/azure-rest-api.model';
 import { NgSwitchDefault } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ import { NgSwitchDefault } from '@angular/common';
 })
 export class AppComponent implements OnInit {
   public instance = Object.values(Instance)
-  
+  public subscription: Subscription;
   public CPULoad: number = 0;
   public HDD1: number = 0;
   public HDD2: number = 0;
@@ -20,15 +21,19 @@ export class AppComponent implements OnInit {
   constructor(public azureRestApiService: AzureRestApiService) { }
   
   ngOnInit(): void {
-      this.switchInstance(Instance.CSMSProduction)
+      this.switchInstance(Instance.CSMSProduction);
   }
 
   
 
   public switchInstance(instance: Instance) {
-    this.azureRestApiService.getInstanceMetric(instance, Metric.CPULoad).subscribe((data) => this.CPULoad = data)
-    this.azureRestApiService.getInstanceMetric(instance, Metric.HDD1RemainingMB).subscribe((data) => this.HDD1 = data)
-    this.azureRestApiService.getInstanceMetric(instance, Metric.HDD2RemainingMB).subscribe((data) => this.HDD2 = data)
-    this.azureRestApiService.getInstanceMetric(instance, Metric.RAMRemainingMB).subscribe((data) => this.RAM = data)
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+
+    this.subscription = this.azureRestApiService.getInstanceMetric(instance, Metric.CPULoad).subscribe((data) => this.CPULoad = data);
+    this.subscription.add(this.azureRestApiService.getInstanceMetric(instance, Metric.HDD1RemainingMB).subscribe((data) => this.HDD1 = data));
+    this.subscription.add(this.azureRestApiService.getInstanceMetric(instance, Metric.HDD2RemainingMB).subscribe((data) => this.HDD2 = data));
+    this.subscription.add(this.azureRestApiService.getInstanceMetric(instance, Metric.RAMRemainingMB).subscribe((data) => this.RAM = data));
   }
 }
