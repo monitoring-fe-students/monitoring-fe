c1710e9b9abdf40b6593e1918217765864f9b03e
Laco <ladislav.dosa@gmail.com>
2023-05-31 08:01:35 +0200

    test case descriptions

--- src/app/pipes/limitcolor.pipe.spec.ts
+++ src/app/pipes/limitcolor.pipe.spec.ts
@@ -1,8 +1,17 @@
 import { LimitcolorPipe } from './limitcolor.pipe';
 
+// simple test cases:
+// 1) transform returns 'red' for Metric.CPULoad when currentValue 5 and limitValue 1
+// 2) transform returns 'black' for Metric.CPULoad when currentValue 1 and limitValue 5
+
+// parametrized test cases:
+// 3) transform returns 'red' for all metrics except Metric.CPULoad when currentValue 1000 and limitValue 900
+// 4) transform returns 'black' for all metrics except Metric.CPULoad when currentValue 500 and limitValue 1000
 describe('LimitcolorPipe', () => {
   it('create an instance', () => {
     const pipe = new LimitcolorPipe();
     expect(pipe).toBeTruthy();
   });
+
+
 });
